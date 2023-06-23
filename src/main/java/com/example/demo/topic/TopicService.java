825e85a862c6615c5f8747eea5a72024ba172292
Shashank <72060531+shashank026@users.noreply.github.com>
2023-06-23 22:03:17 +0530

    Add Post request feature

--- src/main/java/com/example/demo/topic/TopicService.java
+++ src/main/java/com/example/demo/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.example.demo.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring", "Spring Framework", "Spring Description"),
 			new Topic("java-spring", "Java Spring Framework", "Java Spring Description"),
 			new Topic("springboot", "Springboot Framework", "Springboot  Description")
-		);
+		));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -21,4 +22,9 @@ public class TopicService {
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+	
 }
