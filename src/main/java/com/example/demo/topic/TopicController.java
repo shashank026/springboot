5e01bc45c27e5517dee517d8755f16c37a470194
Shashank <72060531+shashank026@users.noreply.github.com>
2023-06-29 13:01:45 +0530

    Finale

--- src/main/java/com/example/demo/topic/TopicController.java
+++ src/main/java/com/example/demo/topic/TopicController.java
@@ -3,7 +3,10 @@ package com.example.demo.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,17 +28,17 @@ public class TopicController {
 		return topicService.getTopic(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	@PostMapping("/topics")
 	public void addTopics(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	@PutMapping("/topics/{id}")
 	public void updateTopics(@RequestBody Topic topic, @PathVariable String id) {
 		topicService.updateTopic(id, topic);
 	} 
 	
-	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	@DeleteMapping("/topics/{id}")
 	public void deleteTopic(@PathVariable String id) {
 		topicService.deleteTopic(id);
 	}
